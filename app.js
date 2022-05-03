cc6b7cced8332bd1d81aab1772ccb23c7434d785
Pınar Suvaçoğlu <37345731+mihalidis@users.noreply.github.com>
2022-05-03 21:26:09 +0300

    update readme.md

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT || 3000}`);
+});
